2750db45218423c3c3b4ba6c1c1c3c5aea8ec871
FacuAce <acevedofacundo410@gmail.com>
2022-08-18 23:44:50 -0300

    Se agrego el modulo RoleUserModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,6 +13,7 @@ import { getCorsConfig } from '@/utils/cors.utils';
 import { PermissionModule } from '@/permission/permission.module';
 import { AreaModule } from '@/area/area.module';
 import { RoleModule } from './role/role.module';
+import { RoleUserModule } from './role-user/role-user.module';
 
 @Module({
   imports: [
@@ -44,6 +45,7 @@ import { RoleModule } from './role/role.module';
     PermissionModule,
     AreaModule,
     RoleModule,
+    RoleUserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
