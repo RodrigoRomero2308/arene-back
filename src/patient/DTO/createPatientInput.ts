44e961d2a418bec5720ea74e75991ff1adbb420b
Rodrigo Romero <rodrigoromero2308@gmail.com>
2022-10-10 15:00:07 -0300

    se agregan campos a modelo de pacientes

--- src/patient/DTO/createPatientInput.ts
+++ src/patient/DTO/createPatientInput.ts
@@ -55,6 +55,24 @@ export class CreatePatientWithoutUserInput {
   @Field({ nullable: true })
   @IsNotEmpty()
   transfer_phone_number?: string;
+  @Field({ nullable: true })
+  cud_number?: string;
+  @Field({ nullable: true })
+  cud_companion?: boolean;
+  @Field({ nullable: true })
+  cud_valid_from?: string;
+  @Field({ nullable: true })
+  cud_valid_to?: string;
+  @Field({ nullable: true })
+  social_work?: string;
+  @Field({ nullable: true })
+  social_work_plan?: string;
+  @Field({ nullable: true })
+  social_work_number?: string;
+  @Field({ nullable: true })
+  social_work_valid_from?: string;
+  @Field({ nullable: true })
+  social_work_valid_to?: string;
 }
 
 @InputType()
